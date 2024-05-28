6b09fe8d91bffc6e15a22c065d1b1a36e0756dd5
Arpit Gaur <arpit.gaur2006@gmail.com>
2024-05-28 18:05:20 +0530

    Remove Pages App

--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from pages import views
-
-urlpatterns = [
-    path('home/', views.home, name='home'),
-]
--- pages/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def home(httprequest):
-    print(httprequest.method)
-    return render(httprequest, "pages/home.html", {})
